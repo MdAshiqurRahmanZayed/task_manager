756fe437dde1c46a04cfc0d65393217a1dffefd4
MdAshiqurRahmanZayed <mdzayed2019@gmail.com>
2023-11-01 11:35:58 +0600

    First commit

---
+++ task_manager/info-demo.py
@@ -0,0 +1,8 @@
+DJANGO_SECRET_PASSWORD = "your-secret-password"
+EMAIL_USE_TLS = True
+EMAIL_HOST    ='smtp.gmail.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587 
+CLIENT_ID_GOOGLE = ""
+SECRET_KEY = ""
